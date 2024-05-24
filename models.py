3537a8327e9de88f74620004b6c92104844fb603
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-05-24 13:44:58 +0530

    New: schemas defined

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -11,18 +11,18 @@ class Intent(Base):
     tag = Column(String, unique=True, index=True)
     response_text = Column(String, index=True)
 
-    text_patterns = relationship("TextPattern", back_populates="intent")
+    input_patterns = relationship("InputPattern", back_populates="intent")
     response_links = relationship("ResponseLink", back_populates="intent")
 
 
-class TextPattern(Base):
-    __tablename__ = "text_patterns"
+class InputPattern(Base):
+    __tablename__ = "input_patterns"
 
     id = Column(Integer, primary_key=True)
     text = Column(String, index=True)
     intent_id = Column(Integer, ForeignKey("intents.id"))
 
-    intent = relationship("Intent", back_populates="text_patterns")
+    intent = relationship("Intent", back_populates="input_patterns")
 
 
 class ResponseLink(Base):
