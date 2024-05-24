491f9faf7c2400f7687318e32316b04466fd44c8
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-05-24 11:23:34 +0530

    Feature: seperate routes add for admin and chatbot

--- main.py
+++ main.py
@@ -6,11 +6,3 @@ from routes.chatbot_api import chatbot
 app = FastAPI()
 app.include_router(admin)
 app.include_router(chatbot)
-
-
-#Done - Create a folder called routes 
-#Done - Add chatbot-api.py and admin-api.py in routes folder
-#Done - Need to figure how to add separate api files in FastAPI
-#Done - Configure route files in main app
-#TODO: Test routes
-#TODO: Start working on db integration
