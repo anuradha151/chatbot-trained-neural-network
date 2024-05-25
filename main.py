c56f6184ae5e31fcb62daa6e77153d8376d940ec
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-05-25 23:48:45 +0530

    Feature: intent creat and find all are added

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 from routes.admin_api import admin
 from routes.chatbot_api import chatbot
 
+from models import Base
+from database import engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 app.include_router(admin)
 app.include_router(chatbot)
--- routes/admin_api.py
+++ routes/admin_api.py
@@ -1,13 +1,33 @@
-from fastapi import APIRouter
+from fastapi import Depends, APIRouter, HTTPException, FastAPI
+from schemas import Intent, IntentCreate
 from training import train
+from repository import find_all_intents, create_intent, find_by_tag
+from database import SessionLocal
+
+from sqlalchemy.orm import Session
 
 admin =  APIRouter()
 
-@admin.post("/admin/tag/create") 
-async def create_tag():
-    return {"message": "Tag created successfully"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-
+# model apis
 @admin.get("/admin/model/train")
 def train_model():
     return train()
+
+@admin.post("/admin/intent/create", response_model=Intent)
+def create_intent_api(intent: IntentCreate, db: Session = Depends(get_db)):
+    db_intent = find_by_tag(db, tag=intent.tag) 
+    if db_intent:
+        raise HTTPException(status_code=400, detail="Tag already created")
+    return create_intent(db=db, intent=intent)
+
+
+@admin.get("/admin/intents", response_model=list[Intent]) 
+def find_all_intents_api(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return find_all_intents(db, skip=skip, limit=limit)
